9e41075c6e32825ceddca65ec4a2b8fcae34e1b4
Max Chistiakov <chistiakovmax@gmail.com>
2022-10-11 05:36:43 +0300

    Algorithm completed

--- Program.cs
+++ Program.cs
@@ -5,3 +5,37 @@
 //=================================================================================
 
 string[] array = new string[] {"GB", "developer", "test", "PM", "WEB"};
+
+// Метод фильтрации массива
+string[] FiltringArray(string[] arr)
+{
+  //вычисляем длину массива result
+  int arrSize = 0;
+  for(int i = 0; i < arr.Length; i++)
+  {
+    if(arr[i].Length <= 3)
+    {
+      arrSize++;
+    }
+  }
+  // Создаем массив и наполняем подходящими элементами
+  string[] result = new string[arrSize];
+  int index = 0;
+  for(int i = 0; i < arr.Length; i++)
+  {
+    if(arr[i].Length <= 3)
+    {
+      result[index] = arr[i];
+      index++;
+    }
+  }
+  return result;
+}
+
+// Выводим массив в консоль
+void Print1DArray(string[] arr)
+{
+    Console.WriteLine($"Результат: [{string.Join(", ", arr)}]");
+}
+
+Print1DArray(FiltringArray(array));
